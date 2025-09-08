47413364122dbca933b3c05e016d6422e65fd0be
HotsixMan <hotsixman05@gmail.com>
2025-09-08 16:15:08 +0900

    version: 0.9.1

--- library/script/xvelte-setup/index.mjs
+++ library/script/xvelte-setup/index.mjs
@@ -7,29 +7,29 @@ import { execSync } from 'node:child_process';
 const projectPathArg = process.argv[2] || 'project';
 const projectPath = path.resolve(projectPathArg);
 
-if(fs.existsSync(projectPath)){
+if (fs.existsSync(projectPath)) {
     const answer = await readLine(`${projectPath} already exists. Remove it and make new project? (y/n)`);
-    if(!isYes(answer)){
+    if (!isYes(answer)) {
         process.exit();
     }
 
-    fs.rmSync(projectPath, {recursive: true, force: true});
+    fs.rmSync(projectPath, { recursive: true, force: true });
 }
 
 console.log('Copying project files...');
-fs.cpSync(path.resolve(import.meta.dirname, 'template'), projectPath, {recursive: true, force: true});
+fs.cpSync(path.resolve(import.meta.dirname, 'template'), projectPath, { recursive: true, force: true });
 
 console.log("Installing packages...");
-execSync("npm install", {cwd: projectPath});
+execSync("npm install", { cwd: projectPath });
 
 console.log('Complete!');
 process.exit();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-function readLine(question){
+function readLine(question) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
     return new Promise((res) => {
         rl.question(question, (answer) => res(answer));
     })
@@ -38,12 +38,12 @@ function readLine(question){
 /**
  * @param {string} y 
  */
-function isYes(y){
+function isYes(y) {
     y = y.toLowerCase();
-    if(y === "y" || y === "yes"){
+    if (y === "y" || y === "yes") {
         return true;
     }
     return false;
 }
 
-export {};
+export { };
